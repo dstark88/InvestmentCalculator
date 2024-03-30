93dc8b12c511d5d7a23835d2b986f035ad9a5c7f
Denise Stark <starkwalker88@gmail.com>
2024-03-29 17:34:43 -0700

    setting up github pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import logo from '../assets/investment-calculator-logo.png';
+import logo from "../assets/investment-calculator-logo.png";
 
 export default function Header() {
   return (
-    <header id="header">
-      <img src={logo} alt="Logo showing a money bag" />
-      <h1>Investment Calculator</h1>
+    <header>
+      <div id="masthead">
+        <a
+          className="navbar-brand"
+          href="https://dstark88.github.io/portfolio/"
+        >
+          Denise Stark
+        </a>
+        <nav className="navbar">
+          <a href="https://dstark88.github.io/portfolio/" target="blank">
+            Portfolio
+          </a>
+        </nav>
+      </div>
+      <div id="header">
+        <img src={logo} alt="Logo showing a money bag" />
+        <h1>Investment Calculator</h1>
+      </div>
     </header>
   );
 }
